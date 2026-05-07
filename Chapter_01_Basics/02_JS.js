663e0040ade773f16c72bde8efaeed287cbd595a
Dipal Prajapati <dipalpra123@gmail.com>
2026-05-07 09:48:44 +0100

    Initial commit from workspace

---
+++ Chapter_01_Basics/02_JS.js
@@ -0,0 +1,8 @@
+let a =10;
+console.log(a);
+
+for(let a=0; a<5; a++)
+    {
+        console.log(a);
+        print(a);
+    }
---
+++ Chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,3 @@
+console.log("node.js version:", process.version);
+console.log("node.js platform:", process.platform);
+console.log("node.js architecture:", process.arch); 
